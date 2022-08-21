5448ece8fae2eff0e8ad14bc4ff9ebbf14d65493
Joey Donovan <jdnvn>
2022-08-21 09:23:39 -0400

    add uuid column option

--- db/migrate/20220820210512_create_users_rooms.rb
+++ db/migrate/20220820210512_create_users_rooms.rb
@@ -1,6 +1,6 @@
 class CreateUsersRooms < ActiveRecord::Migration[6.0]
   def change
-    create_join_table :users, :rooms, column_options: { null: false, foreign_key: true } do |t|
+    create_join_table :users, :rooms, column_options: { null: false, foreign_key: true, type: :uuid } do |t|
       t.index [:user_id, :room_id]
       t.index [:room_id, :user_id]
     end
