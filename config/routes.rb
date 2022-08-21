f0b3dd31f7c3306c53da03f5e376343c73a8f28e
Joey Donovan <jdnvn>
2022-08-21 01:15:39 -0400

    remove /api scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  scope '/api' do
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    post :registrations, to: 'registrations#create'
-    delete :logout, to: 'sessions#logout'
-    get :logged_in, to: 'sessions#logged_in'
-    resources :sessions, only: [:create]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post :registrations, to: 'registrations#create'
+  delete :logout, to: 'sessions#logout'
+  get :logged_in, to: 'sessions#logged_in'
+  resources :sessions, only: [:create]
 
-    resources :rooms do
-      post :enter
-    end
-
-    get 'ably/auth'
+  resources :rooms do
+    post :enter
   end
+
+  get 'ably/auth'
 end
